c9a79d952dca132f837cea6028687d473e4dd05f
Hudson Ayers <hudson.ayers@getcruise.com>
2023-06-14 17:43:38 -0700

    wip

--- append_m7.sh
+++ append_m7.sh
@@ -178,6 +178,12 @@ expected_ep=$(roundup $ram_start $VTABLE_ALIGN)
 m7_bin_padding=$((expected_ep - ram_start))
 m7_bin_off=$((m7_bin_off + m7_bin_padding))
 
+printf "app_start_off: 0x%x\n" "${app_start_off}"
+printf "app_header_off: 0x%x\n" "${app_header_off}"
+printf "ram_start_orig: 0x%x\n" "${ram_start_orig}"
+printf "ram_start: 0x%x\n" "${ram_start}"
+printf "expected_ep: 0x%x\n" "${expected_ep}"
+
 if test "${show_expected_ep}"; then
 	printf "0x%x\n" "${expected_ep}"
 	exit 0
